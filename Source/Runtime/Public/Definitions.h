0a29c7e6e0688b82fcad6ff440d83b434d397a62
MSalholm <myles.salholm@gmail.com>
2015-02-11 02:49:11 -0800

    v0.01 Initial commit.

---
+++ Source/Runtime/Public/Definitions.h
@@ -0,0 +1,13 @@
+/* Copyright 2015 Myles Salholm */
+
+#ifndef GLOBAL_DEFINITIONS
+#define GLOBAL_DEFINITIONS
+
+/* Current engine version. */
+#define TITLE "Madness Engine v0.01"
+#define SHORT_TITLE "MAD"
+#define VERSION 0.01f
+/* If we are compiling for Windows. */
+#define WINDOWS 1
+
+#endif
